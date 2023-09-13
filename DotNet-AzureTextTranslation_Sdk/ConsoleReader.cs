d38d4f883eacfb0a54eef0ecd765be0b721bc612
Vitor Jesus <vitorjdev@gmail.com>
2023-09-12 22:09:45 -0300

    feat: initial commit

---
+++ DotNet-AzureTextTranslation_Sdk/ConsoleReader.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_AzureTextTranslation_Sdk
+{
+    public static class ConsoleReader
+    {
+        public static string GetKey()
+        {
+            Console.WriteLine("What's your translator key?");
+            return ForceReadLine("Enter a valid key...");
+        }
+
+        public static string GetRegion()
+        {
+            Console.WriteLine("What's your translator region?");
+            return ForceReadLine("Enter a valida region...");
+        }
+
+        public static string GetLanguage()
+        {
+            Console.WriteLine("Which language do you want to translate into?");
+            return ForceReadLine("Enter a valid language...");
+        }
+
+        public static string GetText()
+        {
+            Console.WriteLine("What do you want to translate?");
+            return ForceReadLine("Enter a valid text...");
+        }
+
+        private static string ForceReadLine(string? retryMessage = null)
+        {
+            string? content = null;
+
+            do
+            {
+                content = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(retryMessage) && string.IsNullOrWhiteSpace(content))
+                {
+                    Console.WriteLine(retryMessage);
+                }
+
+            } while (string.IsNullOrWhiteSpace(content));
+
+            return content;
+        }
+    }
+}
---
+++ DotNet-AzureTextTranslation_Sdk/Program.cs
@@ -0,0 +1,30 @@
+﻿using Azure;
+using Azure.AI.Translation.Text;
+using DotNet_AzureTextTranslation_Sdk;
+
+var key = ConsoleReader.GetKey();
+var region = ConsoleReader.GetRegion();
+
+while (true)
+{
+    try
+    {
+        var language = ConsoleReader.GetLanguage();
+        var text = ConsoleReader.GetText();
+
+        var credential = new AzureKeyCredential(key);
+        var client = new TextTranslationClient(credential, region);
+
+        var translations = (await client.TranslateAsync(language, text)).Value;
+        var translatedText = translations
+            ?.FirstOrDefault()
+            ?.Translations
+            ?.FirstOrDefault()
+            ?.Text;
+
+        Console.WriteLine("Translated text: " + translatedText);
+    } catch
+    {
+        Console.WriteLine("...");
+    }
+}
